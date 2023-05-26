612c508adc3f31719fdb069709e33bce18e6261d
jonatas.paz <jonatas.paz@imusica.com.br>
2023-05-26 19:45:08 -0300

    implementação de formularios reativos

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   loginCard = true;
-  constructor() { }
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  })
+  constructor(private formBuilder: FormBuilder) { }
+
+  onSubmitLoginForm(){
+    console.log("Dados do formulario de login", this.loginForm.value)
+  }
+
+  onSubmitSignupForm(){
+    console.log("Dados do formulario de criação de conta", this.signupForm.value)
+  }
+
 
   ngOnInit() {
   }
